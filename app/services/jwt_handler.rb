1064af6939482a4203c0c2f37b7be8894566daa7
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-11 21:45:49 +0100

    Added jwt for token handling

---
+++ app/services/jwt_handler.rb
@@ -0,0 +1,13 @@
+class JwtHandler
+  HMAC_SECRET = Rails.application.secrets.secret_key_base
+
+  def self.encode(payload)
+    JWT.encode(payload, HMAC_SECRET)
+  end
+
+  def self.decode(token)
+    decoded_token = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndiferentAccess.new(decoded_token)
+    # TODO: Add error handling
+  end
+end
