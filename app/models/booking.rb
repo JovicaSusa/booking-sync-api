c77520c07ac343125123429e5d1c3945513e8973
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-12 11:53:12 +0100

    Added booking model

---
+++ app/models/booking.rb
@@ -0,0 +1,5 @@
+class Booking < ApplicationRecord
+  belongs_to :rental
+
+  validates_presence_of :start_at, :end_at, :client_email, :price, :rental
+end
---
+++ spec/factories/bookings.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :booking do
+    start_at "2017-11-12 11:48:52"
+    end_at "2017-11-12 11:48:52"
+    client_email "MyString"
+    price "9.99"
+    rental nil
+  end
+end
