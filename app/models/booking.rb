c06106439ce7f9788a51a43672dacb2e6c5cb21c
Jovica Susa <jovicasusa1985@gmail.com>
2017-11-19 20:56:15 +0100

    Added validations for booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,34 @@
 class Booking < ApplicationRecord
   belongs_to :rental
 
+  MIN_STAY = 1
+
   validates_presence_of :start_at, :end_at, :client_email, :price, :rental
+  validate :validate_overlap
+  validate :validate_period
+
+  private
+
+  def validate_overlap
+    possible_overlap = rental.bookings.any? { |booking| check_overlap(booking) }
+
+    if possible_overlap
+      errors.add(:start_at, 'Rental is already booked for this date')
+      errors.add(:end_at, 'Rental is already booked for this date')
+    end
+  end
+
+  def validate_period
+    booking_period = ((end_at - start_at) / 1.day).round
+
+    if booking_period <= MIN_STAY
+      errors.add(:start_at, 'Booking period must be at least one/night day')
+      errors.add(:end_at, 'Booking period must be at least one/night day')
+    end
+  end
+
+  def check_overlap(booking)
+    start_at > booking.start_at && start_at < booking.end_at ||
+    booking.start_at > start_at && booking.start_at < end_at
+  end
 end
